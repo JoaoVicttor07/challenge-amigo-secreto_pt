b4660f50a33b4266d8573f6eb49239d54f10fd77
João Victor <joaovictorsdc07@gmail.com>
2025-03-12 21:44:58 -0300

    Final commit

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let campoInput = document.getElementById('amigo').value;
+    amigos.push(campoInput);
+
+    if (campoInput === '') {
+        alert('Por favor, insira um nome.');
+        return;
+    }
+    document.getElementById('amigo').value = '';
+    document.getElementById('listaAmigos').innerHTML = amigos.join('<br>');
+}
+
+function sortearAmigo() {
+    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `O amigo secreto sorteado foi: ${amigoSorteado}`;
+    document.getElementById('listaAmigos').innerHTML = '';
+    amigos = [];
+
+}
